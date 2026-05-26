be689d7fefebb78a6fff559183a92170c2045278
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-05-26 15:06:42 +0530

    checking goat

---
+++ count.js
@@ -0,0 +1,9 @@
+import { kv } from '@vercel/kv';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  const count = await kv.incr('visits');
+  res.json({ count });
+}
