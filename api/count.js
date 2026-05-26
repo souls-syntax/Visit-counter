0238a2af87a6baed90e742f592863b9accf3f582
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-05-26 15:22:13 +0530

    hmm

--- api/count.js
+++ api/count.js
@@ -1,9 +1,10 @@
-import { kv } from '@vercel/kv';
+import { Redis } from '@upstash/redis';
+
+const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-  const count = await kv.incr('visits');
+  const count = await redis.incr('visits');
   res.json({ count });
 }
